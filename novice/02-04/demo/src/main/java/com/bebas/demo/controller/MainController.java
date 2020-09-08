e729e0e2c2d4bb76a028ea95a4ae1157943c075b
inaii <mutmainahh07@gmail.com>
2020-09-08 11:03:06 +0700

    update spring CRUD

--- novice/02-04/demo/src/main/java/com/bebas/demo/controller/MainController.java
+++ novice/02-04/demo/src/main/java/com/bebas/demo/controller/MainController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,12 +19,32 @@ public class MainController {
     @Autowired
     private UserRepository userRepository;
    
-    @PostMapping(path="/add") // Map ONLY POST Requests
+@PostMapping(path="/add") // Map ONLY POST Requests
 public @ResponseBody User addNewUser(@RequestBody User user) {  
     return userRepository.save(user);
 }
-    @GetMapping(path="/all")
-    public @ResponseBody Iterable<User> getAlllUsers() {
-        return userRepository.findAll();
+@GetMapping(path="/all")
+public @ResponseBody Iterable<User> getAlllUsers() {
+    return userRepository.findAll();
+
     }
+@DeleteMapping("/delete/{id}")
+public void deleteUser(@PathVariable Integer id){
+    userRepository.deleteById(id);
+}
+@PutMapping("/update/{id}")
+User updatedUser(@RequestBody User newUser, @PathVariable Integer id){
+ 
+    return userRepository.findById(id)
+    .map(user -> {
+        user.setName(newUser.getName());
+        user.setEmail(newUser.getEmail());
+        return userRepository.save(user);
+      })
+      .orElseGet(() -> {
+        newUser.setId(id);
+        return userRepository.save(newUser);
+      });
+  }
+   
 }
